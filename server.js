b302696e5730241a27d9a6df7ee01d25ea75626a
Chilace <22879802+Chilace@users.noreply.github.com>
2017-12-17 00:14:25 +0300

    Add express to serve feeds

---
+++ server.js
@@ -0,0 +1,17 @@
+require('./tweetstorss.js');
+var config = require('./config.json');
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send(config.items);
+});
+
+app.get('/:feed', function (req, res) {
+  res.sendFile(__dirname + '/.data/feeds/' + req.params.feed);
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
